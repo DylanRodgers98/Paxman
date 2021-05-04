2e96b5061a059bd53b8640652d7db4909eb4821c
Dylan Rodgers <dylanirodgers@aol.com>
2021-05-04 14:48:42 +0100

    Fix Paxman's movement so he stops when colliding with wall, without breaking movement

--- Assets/Scripts/PlayerController.cs
+++ Assets/Scripts/PlayerController.cs
@@ -4,13 +4,14 @@ using UnityEngine.InputSystem;
 public class PlayerController : MonoBehaviour
 {
     [SerializeField] private float movementSpeed;
-    private Vector2 _moveDirection = Vector2.left;
-    
+    private Vector2 _movementDirection = Vector2.left;
+    private Vector3 _lastKnownPosition;
+
     private void Move(InputAction.CallbackContext context)
     {
         if (context.performed)
         {
-            _moveDirection = context.ReadValue<Vector2>();
+            _movementDirection = context.ReadValue<Vector2>();
             SetScale();
             SetRotation();
         }
@@ -18,22 +19,22 @@ public class PlayerController : MonoBehaviour
 
     private void SetScale()
     {
-        transform.localScale = _moveDirection == Vector2.right 
-            ? new Vector3(-1, 1, 1) 
+        transform.localScale = _movementDirection == Vector2.right
+            ? new Vector3(-1, 1, 1)
             : Vector3.one;
     }
 
     private void SetRotation()
     {
-        if (_moveDirection == Vector2.left || _moveDirection == Vector2.right)
+        if (_movementDirection == Vector2.left || _movementDirection == Vector2.right)
         {
             transform.rotation = Quaternion.identity;
         }
-        else if (_moveDirection == Vector2.up)
+        else if (_movementDirection == Vector2.up)
         {
             transform.rotation = Quaternion.Euler(0, 0, -90);
         }
-        else if (_moveDirection == Vector2.down)
+        else if (_movementDirection == Vector2.down)
         {
             transform.rotation = Quaternion.Euler(0, 0, 90);
         }
@@ -41,21 +42,27 @@ public class PlayerController : MonoBehaviour
 
     private void Update()
     {
-        MoveCharacter();
+        if (!IsStationary()) UpdatePosition();
     }
 
-    private void MoveCharacter()
+    private bool IsStationary()
     {
-        if (_moveDirection == Vector2.zero) return;
-        Vector3 currentPosition = transform.position;
+        if (_movementDirection == Vector2.zero) return true;
+        if (transform.position == _lastKnownPosition)
+        {
+            _movementDirection = Vector2.zero;
+            return true;
+        }
+
+        return false;
+    }
+
+    private void UpdatePosition()
+    {
+        _lastKnownPosition = transform.position;
         float distanceToMove = movementSpeed * Time.deltaTime;
-        float x = currentPosition.x + _moveDirection.x * distanceToMove;
-        float y = currentPosition.y + _moveDirection.y * distanceToMove;
+        float x = _lastKnownPosition.x + _movementDirection.x * distanceToMove;
+        float y = _lastKnownPosition.y + _movementDirection.y * distanceToMove;
         transform.position = new Vector2(x, y);
-    }
-
-    private void OnCollisionEnter2D(Collision2D other)
-    {
-        _moveDirection = Vector2.zero;
     }
 }
