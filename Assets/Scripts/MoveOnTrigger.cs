f7945ea748ce7afe87f1746097d57383cf2d6aef
Dylan Rodgers <dylanirodgers@aol.com>
2021-05-04 15:30:39 +0100

    Allow characters to teleport to other side of level

---
+++ Assets/Scripts/MoveOnTrigger.cs
@@ -0,0 +1,11 @@
+﻿using UnityEngine;
+
+public class MoveOnTrigger : MonoBehaviour
+{
+    [SerializeField] private Transform destinationTransform;
+
+    private void OnTriggerEnter2D(Collider2D other)
+    {
+        other.transform.position = destinationTransform.position;
+    }
+}
