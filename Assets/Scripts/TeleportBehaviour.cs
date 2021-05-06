7a29fe57b8d32f9bf3703ef4719063b3b0b448fa
Dylan Rodgers <dylanirodgers@aol.com>
2021-05-06 12:43:13 +0100

    Rename and refactor teleport script

---
+++ Assets/Scripts/TeleportBehaviour.cs
@@ -0,0 +1,12 @@
+﻿using UnityEngine;
+
+[RequireComponent(typeof(Collider2D))]
+public class TeleportBehaviour : MonoBehaviour
+{
+    [SerializeField] private Transform teleportDestination;
+
+    private void OnTriggerEnter2D(Collider2D other)
+    {
+        other.transform.position = teleportDestination.position;
+    }
+}
