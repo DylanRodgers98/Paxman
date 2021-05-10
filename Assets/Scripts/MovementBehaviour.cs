a1ff6b18c77a665e34cfed305a2b90bb454613d7
Dylan Rodgers <dylanirodgers@aol.com>
2021-05-10 18:56:56 +0100

    Refactor MovementBehaviour

--- Assets/Scripts/MovementBehaviour.cs
+++ Assets/Scripts/MovementBehaviour.cs
@@ -3,25 +3,31 @@
 public class MovementBehaviour : MonoBehaviour
 {
     [SerializeField] private float movementSpeed;
+    private Transform _transform;
+    private Vector2 _lastKnownPosition;
     private float _distanceToMove;
     private float _x;
     private float _y;
 
     protected Vector2 MovementDirection { get; private set; }
-    protected Vector2 LastKnownPosition { get; private set; }
 
     protected virtual void SetMovementDirection(Vector2 movementDirection)
     {
         MovementDirection = movementDirection;
     }
 
+    private void Awake()
+    {
+        _transform = transform;
+    }
+
     private void Update()
     {
         if (MovementDirection == Vector2.zero) return;
-        LastKnownPosition = transform.position;
+        _lastKnownPosition = _transform.position;
         _distanceToMove = movementSpeed * Time.deltaTime;
-        _x = LastKnownPosition.x + MovementDirection.x * _distanceToMove;
-        _y = LastKnownPosition.y + MovementDirection.y * _distanceToMove;
-        transform.position = new Vector2(_x, _y);
+        _x = _lastKnownPosition.x + MovementDirection.x * _distanceToMove;
+        _y = _lastKnownPosition.y + MovementDirection.y * _distanceToMove;
+        _transform.position = new Vector2(_x, _y);
     }
 }
