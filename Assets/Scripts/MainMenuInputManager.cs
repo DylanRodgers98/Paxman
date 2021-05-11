598f9aeaebb3b5c0e2f11720f3e136714aab5edc
Dylan Rodgers <dylanirodgers@aol.com>
2021-05-11 23:00:40 +0100

    Create scenes for viewing leaderboard and replays

--- Assets/Scripts/MainMenuInputManager.cs
+++ Assets/Scripts/MainMenuInputManager.cs
@@ -4,11 +4,17 @@ using UnityEngine.SceneManagement;
 public class MainMenuInputManager : MonoBehaviour
 {
     [SerializeField] private string gameSceneName;
+    [SerializeField] private string viewLeaderboardSceneName;
     [SerializeField] private string viewReplaysSceneName;
 
     public void OnStartButtonClick()
     {
         SceneManager.LoadScene(gameSceneName);
+    }
+
+    public void OnViewLeaderboardButtonClick()
+    {
+        SceneManager.LoadScene(viewLeaderboardSceneName);
     }
 
     public void OnViewReplaysButtonClick()
