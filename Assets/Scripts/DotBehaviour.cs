2432dbba8dc0d1a55cf0ead048b5b359cd8f8e21
Dylan Rodgers <dylanirodgers@aol.com>
2021-05-11 11:43:56 +0100

    Remove EdibleByPlayer abstract class

--- Assets/Scripts/DotBehaviour.cs
+++ Assets/Scripts/DotBehaviour.cs
@@ -1,13 +1,22 @@
 ﻿using System;
 using UnityEngine;
 
-public class DotBehaviour : EdibleByPlayer
+[RequireComponent(typeof(Collider2D))]
+public class DotBehaviour : MonoBehaviour
 {
     public static event Action<int> OnDotEaten;
     [SerializeField] private int score;
     private PacDotManager _pacDotManager;
+    
+    protected virtual void OnTriggerEnter2D(Collider2D other)
+    {
+        // check isTrigger because player character has two colliders:
+        // one that checks for collisions against walls (non-trigger),
+        // and another that checks for collisions with dots (trigger).
+        if (other.CompareTag("Player") && other.isTrigger) Eat();
+    }
 
-    protected override void Eat()
+    protected virtual void Eat()
     {
         OnDotEaten?.Invoke(score);
         gameObject.SetActive(false);
