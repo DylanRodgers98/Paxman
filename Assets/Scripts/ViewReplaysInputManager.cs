7a281b3e8ebb2c28bf3b0efdce3e420c876df40a
Dylan Rodgers <dylanirodgers@aol.com>
2021-05-11 23:16:03 +0100

    Allow navigation between scenes

---
+++ Assets/Scripts/ViewReplaysInputManager.cs
@@ -0,0 +1,14 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class ViewReplaysInputManager : MonoBehaviour
+{
+    [SerializeField] private string mainMenuSceneName;
+
+    public void OnBackButtonClick()
+    {
+        SceneManager.LoadScene(mainMenuSceneName);
+    }
+}
