8bc18fb0e865cc518c3dd196500618681e1148cf
Dylan Rodgers <dylanirodgers@aol.com>
2021-05-11 12:49:39 +0100

    Create super basic main menu

---
+++ Assets/Scripts/MainMenuInputManager.cs
@@ -0,0 +1,17 @@
+﻿using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class MainMenuInputManager : MonoBehaviour
+{
+    [SerializeField] private string gameSceneName;
+
+    public void OnStartButtonClick()
+    {
+        SceneManager.LoadScene(gameSceneName);
+    }
+
+    public void OnQuitButtonClick()
+    {
+        Application.Quit();
+    }
+}
