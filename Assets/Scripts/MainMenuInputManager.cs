fe20edbc0b880708f82f2a724420eff64ea94a6f
Dylan Rodgers <dylanirodgers@aol.com>
2021-05-11 17:38:52 +0100

    Add button to main menu to view replays

--- Assets/Scripts/MainMenuInputManager.cs
+++ Assets/Scripts/MainMenuInputManager.cs
@@ -4,10 +4,16 @@ using UnityEngine.SceneManagement;
 public class MainMenuInputManager : MonoBehaviour
 {
     [SerializeField] private string gameSceneName;
+    [SerializeField] private string viewReplaysSceneName;
 
     public void OnStartButtonClick()
     {
         SceneManager.LoadScene(gameSceneName);
+    }
+
+    public void OnViewReplaysButtonClick()
+    {
+        SceneManager.LoadScene(viewReplaysSceneName);
     }
 
     public void OnQuitButtonClick()
